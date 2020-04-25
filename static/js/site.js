ae1f86f601f4a6cdca248ab3a4c78832fd218c87
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2020-04-25 18:31:14 +0700

    post: add thumbnail source

--- static/js/site.js
+++ static/js/site.js
@@ -1,16 +1,3 @@
-$(document).scroll(function () {
-  $('#share-button').toggle($(this).scrollTop() > 300);
-});
-
-$(window).scroll(function () {
-  var offsetTop = $("#share-page").offset() && $("#share-page").offset().top;
-  var scrollTop = $(window).scrollTop();
-  var target = document.getElementById("share-button")
-  if (target) {
-    scrollTop > offsetTop - 700 ? target.style.opacity = 0 : target.style.opacity = 1
-  }
-});
-
 $(document).ready(function () {
   $("#btn-nav").click(function () {
     if ($("#navbarSupportedContent").attr("class") == "collapse navbar-collapse") {
